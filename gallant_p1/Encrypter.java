438acfb74d468e3efce5512816c03acc9bbc6369
DarthofLuke <gallant.thomas@outlook.com>
2020-02-01 20:52:41 -0500

    first commit

---
+++ gallant_p1/Encrypter.java
@@ -0,0 +1,10 @@
+package gallant_p1;
+
+public class Encrypter {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ gallant_p2/BMICalculator.java
@@ -0,0 +1,69 @@
+package gallant_p2;
+
+public class BMICalculator {
+
+	public static void main(String[] args) {
+		BMICalculator app = new BMICalculator();
+		app.readUserData();
+		app.calculateBmi();
+		app.displayBmi();
+	}
+	
+	public void readUserData() {
+		// makes use of both readUnitType and readMeasuementData methods
+	}
+	
+	private void readUnitType() {
+		
+	}
+	
+	private void readMeasurementData() {
+		//calls either readMetricData or readImperialData method
+	}
+	
+	private void readMetricData() {
+		
+	}
+	
+	private void readImperialData() {
+		
+	}
+	
+	public void calculateBmi() {
+		
+	}
+	
+	private void calculateBmiCategory() {
+		
+	}
+	
+	public void displayBmi() {
+		
+	}
+	
+	public void getWeight() {
+		
+	}
+	
+	private void setWeight() {
+		
+	}
+	
+	public void getHeight() {
+		
+	}
+	
+	private void setHeight() {
+		
+	}
+	
+	public void getBmi() {
+		
+	}
+	
+	public void getBmiCategory() {
+		
+	}
+	
+	
+}
